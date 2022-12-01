9c5cfa4cc3fc6419d79c6356fc9995045c8e7191
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-12-01 20:34:13 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import SearchIcon from './searchicon.svg'
 import Movie from './Movie';
 
 const API_URL = 'http://www.omdbapi.com?apikey=197e64bb';
-const movie1 = {
-  "Title": "Batman v Superman: Dawn of Justice",
-  "Year": "2016",
-  "imdbID": "tt2975590",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
 
-function App() {
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  
 
   const searchMovies = async (title) =>{
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json()
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -30,21 +27,38 @@ function App() {
       <div className='search'>
         <input
           placeholder='Search for movies'
-          value = 'Batman'
-          onChange={() => {}}
+          value = {searchTerm}
+          onChange={(e) => { setSearchTerm(e.target.value)}}
         />
         <img 
           src={SearchIcon}
           alt = 'search'
-          onClick={() => {}}
+          onClick={() => {searchMovies(searchTerm)}}
         />
       </div>
-      <div className='container'>
-        <Movie movie1 = {movie1} />
-      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className='container'>
+             {movies.map((movie) => (
+               <Movie movie = {movie} />
+             ))}
+            </div>
+
+          ) : 
+          (
+
+          <div className='empty'>
+            <h2>No movies found!</h2>
+          </div>
+          )
+      }
+      
     </div>
     
   );
+  
 }
 
 export default App;
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const Movie = ({movie1}) => {
+const Movie = ({movie}) => {
   
   return(
       <div className='movie'>
       <div>
-        <p>{movie1.Year}</p>
+        <p>{movie.Year}</p>
       </div>
       <div>
-        <img src = {movie1.Poster !== 'NA' ? movie1.Poster: 'https://via.placeholder.com/400'} alt = {movie1.title} />
+        <img src = {movie.Poster !== 'N/A' ? movie.Poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/800px-No-Image-Placeholder.svg.png'} alt = {movie.title} />
       </div>
       <div>
-        <span>{movie1.Type}</span>
-        <h3>{movie1.Title}</h3>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
       </div>
 
     </div>
